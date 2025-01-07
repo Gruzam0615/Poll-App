6dca06d7894b6f141e90d405a0ea49ec036646f6
gruzam0615 <gruzam0615@gmail.com>
2025-01-07 23:01:11 +0900

    /poll/polling/[pollIndex] 에서 투표하기 기능 개발중중

--- front/app/poll/polling/[pollIndex]/page.tsx
+++ front/app/poll/polling/[pollIndex]/page.tsx
@@ -7,32 +7,34 @@ import { useEffect, useState } from "react";
 import { findByPollIndex } from "@/app/api/poll_server";
 
 export default function Page() {
-    const pathname = usePathname(); 
+    const pathname = usePathname();
     const params = useSearchParams();
-    const [ candidateList, setCandidateList ] = useState([]);
+    const [pollData, setPollData] = useState([]);
 
     const onSubmitAction = (event: any) => {
         event.preventDefault();
-        // if (event.target.candidates.value === undefined || event.target.candidates.value === "") {
-        //     alert("아무것도 선택하지 않았습니다.");
-        // } else {
-        //     let tempNumber = Number(event.target.candidates.value);
-        //     console.log(`${typeof (tempNumber)}, ${tempNumber}`);
-        //     const updatePollResult = pollResult.map((v, i) => {
-        //         if (i === tempNumber - 1) {
-        //             return {
-        //                 "name": v.name,
-        //                 "countValue": v.countValue + 1
-        //             }
-        //         } else {
-        //             return v;
-        //         }
-        //     });
-        //     setPollResult(updatePollResult);
-        //     // alert(`투표 완료\npollResult: ${JSON.stringify(pollResult)}`)
-        //     alert(`투표 완료`);
-        //     router.push("/poll");
-        // }
+        const selectedValue = event.target.pollCandidates.value;
+        // console.log(event.target.pollCandidates.value);
+        if(selectedValue === undefined || selectedValue === "") {
+            alert("아무것도 선택하지 않았습니다.");
+        } else {
+            // let tempNumber = Number(event.target.pollCandidates.value);
+            // console.log(`${typeof (tempNumber)}, ${tempNumber}`);
+            // const updatePollResult = pollResult.map((v, i) => {
+            //     if (i === tempNumber - 1) {
+            //         return {
+            //             "name": v.name,
+            //             "countValue": v.countValue + 1
+            //         }
+            //     } else {
+            //         return v;
+            //     }
+            // });
+            // setPollResult(updatePollResult);
+            // // alert(`투표 완료\npollResult: ${JSON.stringify(pollResult)}`)
+            // alert(`투표 완료`);
+            // router.push("/poll");
+        }
     }
 
     useEffect(() => {
@@ -41,7 +43,9 @@ export default function Page() {
             const body = pollIndex;
 
             const result = await findByPollIndex(body);
-            console.log(result);
+            const data = await result.data;
+            setPollData(data.pollCandidateList);
+            console.log(data.pollCandidateList);
         }
         searchData();
     }, [])
@@ -51,25 +55,23 @@ export default function Page() {
             <div className="grid grid-cols-1 w-full self-center">
                 <h1 className="text-6xl">{params.get("pollName")}</h1>
             </div>
+            <form onSubmit={onSubmitAction}>
             <div className="grid grid-cols-1 w-full h-32 self-center">
-                {/* <form className="py-10" name="candidates" onSubmit={onSubmitAction}>
-                    <fieldset className="flex max-w-md flex-col gap-4">
-                        <legend className="mb-4">후보</legend>
-                        {
-                            // candidatesList.map((candidate, index: number) => (
-                            //     <div key={index} className="flex items-center gap-2">
-                            //         <Radio id={candidate.id} name="candidates" value={candidate.index} />
-                            //         <Label htmlFor={candidate.id}>{candidate.value}</Label>
-                            //     </div>
-                            // ))
-                        }
-                    </fieldset>
-                    <Button className="my-5" color="dark" type="submit">투표하기</Button>
-                </form> */}
+                <fieldset className="flex max-w-md flex-col gap-4">
+                {
+                    pollData?.map((item: any, index: number) => (
+                        <div key={index} className="flex items-center gap-2">
+                            <Radio id={item.pollCandidateName} name="pollCandidates" value={item.pollCandidateName} />
+                            <Label htmlFor={item.pollCandidateName}>{item.pollCandidateName}</Label>
+                        </div>
+                    ))
+                }
+                </fieldset>
             </div>
             <div className="grid grid-cols-1 w-full self-center">
                 <Button className="my-5" color="dark" type="submit">투표하기</Button>
             </div>
+            </form>
         </div>
     );
 }
